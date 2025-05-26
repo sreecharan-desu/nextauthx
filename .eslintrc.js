b1882166c04c6b6caa5758d7b7102978c292014e
SreeCharan Desu <sreecharan309@gmail.com>
2025-05-26 16:10:48 +0530

    skipped/ linting in pod

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // other config like env, extends, parserOptions...
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+}
